d1153901ba7456592fade42f8403c510f1222964
Michael Gale <michael@michaelgale.dev>
2020-05-12 08:37:21 +1000

    initial commit

---
+++ example.py
@@ -0,0 +1,40 @@
+from tkinter import *
+from tkinter import ttk
+def calculate(*args):
+    try:
+        value = float(fahrenheit.get())
+        celsius.set((value -32) * (5 / 9))
+    except ValueError:
+        pass
+
+root = Tk()
+root.title('Fahrenheit to Celsius')
+
+frame = ttk.Frame(root, padding='3 3 12 12')
+frame.grid(column=0, row=0, sticky=(N, W, E, S))
+frame.columnconfigure(0, weight=1)
+frame.rowconfigure(0, weight=1)
+
+fahrenheit = StringVar()
+celsius = StringVar()
+
+f_entry = ttk.Entry(frame, width=7, textvariable=fahrenheit)
+f_entry.grid(column=2, row=1, sticky=(W, E))
+
+c_label = ttk.Label(frame, textvariable=celsius)
+c_label.grid(column=2, row=2, sticky=(W, E))
+g_button = ttk.Button(frame, text='Go', command=calculate)
+g_button.grid(column=3, row=3, sticky=W)
+f_label = ttk.Label(frame, text='degrees F')
+f_label.grid(column=3, row=1, sticky=W)
+e_label = ttk.Label(frame, text='is equivalent to')
+e_label.grid(column=1, row=2, sticky=E)
+d_label = ttk.Label(frame, text='degrees C')
+d_label.grid(column=3, row=2, sticky=W)
+
+for child in frame.winfo_children():
+    child.grid_configure(padx=5, pady=5)
+
+f_entry.focus()
+root.bind('<Return>', calculate)
+root.mainloop()
