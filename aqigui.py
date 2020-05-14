5643797d72228f005e25f604a2082dca19e0f344
Michael Gale <michael@michaelgale.dev>
2020-05-15 08:09:32 +1000

    icon lines

---
+++ aqigui.py
@@ -0,0 +1,20 @@
+from tkinter import Tk, Label, Button, PhotoImage
+
+## TK Setup
+root = Tk()
+root.title('AQIGUI')
+img = PhotoImage(file='FrameIcon.png') # Set window icon
+root.tk.call('wm', 'iconphoto', root._w, img)
+##################################################
+## App Start!
+
+myLabel = Label(root,text='test')
+myLabel.grid(row=0,column=0)
+
+
+
+
+
+
+##################################################
+root.mainloop()
